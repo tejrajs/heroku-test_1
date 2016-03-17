417592fae514730034bdc5e67d9e4fb09d8ac3bf
Tej Raj Shrestha <helloteju96@gmail.com>
2016-03-18 02:26:06 +0545

    added facebook sdk-4

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
-echo "Hello World";
-?>
+
+$fb = new Facebook\Facebook([
+  'app_id' => '192495190824190',
+  'app_secret' => '07b067bb8867e10bc11636fa4bb6f49d',
+  'default_graph_version' => 'v2.5',
+  //'default_access_token' => '{access-token}', // optional
+]);
+
+// Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
+//   $helper = $fb->getRedirectLoginHelper();
+//   $helper = $fb->getJavaScriptHelper();
+//   $helper = $fb->getCanvasHelper();
+//   $helper = $fb->getPageTabHelper();
+
+try {
+  // Get the Facebook\GraphNodes\GraphUser object for the current user.
+  // If you provided a 'default_access_token', the '{access-token}' is optional.
+  $response = $fb->get('/me', '{access-token}');
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$me = $response->getGraphUser();
+echo 'Logged in as ' . $me->getName();
