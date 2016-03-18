55283f66af49ead433304ac363a770c272ae8956
Tej Raj Shrestha <tej.raj@bentraytech.com>
2016-03-18 12:33:14 +0545

    feed premission added

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 require(__DIR__ . '/config.php');
 
 $helper = $fb->getRedirectLoginHelper();
-$permissions = ['email', 'user_likes']; // optional
+$permissions = ['email', 'user_likes','feed']; // optional
 $loginUrl = $helper->getLoginUrl('https://tejtestapp.herokuapp.com/login-callback.php', $permissions);
 
 echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
