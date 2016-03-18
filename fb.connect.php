eb5faccfcbaa5f18eef6c046aa01b67c979128a2
Tej Raj Shrestha <tej.raj@bentraytech.com>
2016-03-18 11:54:01 +0545

    login session bug fixed

--- fb.connect.php
+++ fb.connect.php
@@ -1,6 +1,20 @@
 <?php
 if(!isset($_SESSION['facebook_access_token'])){
-	header('Location: login-callback.php');
+	$helper = $fb->getRedirectLoginHelper();
+	try {
+		$accessToken = $helper->getAccessToken();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		echo 'Graph returned an error: ' . $e->getMessage();
+		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+	if (isset($accessToken)) {
+		header('Location: login-callback.php');
+	}
 }else{
 	$accessToken = $_SESSION['facebook_access_token'];
 }
