abc48021dd1500bea33c41e8a45a889155b3f0b4
Tej Raj Shrestha <tej.raj@bentraytech.com>
2016-03-18 11:39:10 +0545

    redirection bug fixed

--- fb.connect.php
+++ fb.connect.php
@@ -1,7 +1,6 @@
 <?php
 if(!isset($_SESSION['facebook_access_token'])){
-	echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
-	exit;
+	header('Location: login-callback.php');
 }else{
 	$accessToken = $_SESSION['facebook_access_token'];
 }
